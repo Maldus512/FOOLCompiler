8eeaa305906a10f2dcb0daf23d1ff9521f14bbbb
aleeraser <alezini94@hotmail.it>
2017-06-26 11:39:47 +0200

    Saved ParNode.java

--- ast/ParNode.java
+++ ast/ParNode.java
@@ -7,41 +7,41 @@ import util.SemanticError;
 
 public class ParNode implements Node {
 
-  private String id;
-  private Node type;
-  
-  public ParNode (String i, Node t) {
-   id=i;
-   type=t;
-  }
-  
-  public String getId(){
-	  return id;
-  }
-  
-  public Node getType(){
-	  return type;
-  }
-  
-  @Override
+	private String id;
+	private Node type;
+	
+	public ParNode (String i, Node t) {
+		id=i;
+		type=t;
+	}
+	
+	public String getId(){
+		return id;
+	}
+	
+	public Node getType(){
+		return type;
+	}
+	
+	@Override
 	public ArrayList<SemanticError> checkSemantics(Environment env) {
 
-	  return new ArrayList<SemanticError>();
+		return new ArrayList<SemanticError>();
 	}
-  
-  public String toPrint(String s) {
-	  return s+"Par:" + id +"\n"
+	
+	public String toPrint(String s) {
+		return s+"Par:" + id +"\n"
 			 +type.toPrint(s+"  ") ; 
-  }
-  
-  //non utilizzato
-  public Node typeCheck () {
-     return null;
-  }
-  
-  //non utilizzato
-  public String codeGeneration() {
+	}
+	
+	//non utilizzato
+	public Node typeCheck () {
+		 return null;
+	}
+	
+	//non utilizzato
+	public String codeGeneration() {
 		return "";
-  }
-    
+	}
+		
 }  
