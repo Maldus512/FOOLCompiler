8d1b3e64aaa1a2bd1c3263503358b4c7c29aaa22
Mattia Maldini <mattia512maldini@gmail.com>
2017-06-27 19:55:20 +0200

    added type void

---
+++ ast/types/VoidTypeNode.java
@@ -0,0 +1,17 @@
+package ast.types;
+
+import java.util.ArrayList;
+
+import util.Environment;
+import util.SemanticError;
+
+public class VoidTypeNode extends TypeNode {
+  
+  public VoidTypeNode () {
+  }
+  
+  @Override
+  public String toPrint(String s) {
+	return s+"Void\n";  
+  }
+}  
