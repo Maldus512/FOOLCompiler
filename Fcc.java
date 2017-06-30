15344b3efd8c256462fa10a6588d1d09cff9ebcd
Maldus512 <mattia512maldini@gmail.com>
2017-06-30 15:24:57 +0200

    Moved Command line parser to internal class

--- Fcc.java
+++ Fcc.java
@@ -1,4 +1,3 @@
-import util.Cli;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -9,7 +8,6 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.IOException;
-
 import parser.ExecuteVM;
 import parser.FOOLLexer;
 import parser.FOOLParser;
@@ -21,16 +19,22 @@ import util.SyntaxErrorListener;
 import ast.FoolVisitorImpl;
 import ast.Node;
 
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Fcc {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Cli commandArgs = new Cli();
+		Cli commandArgs = new Cli();
 
-        commandArgs.parse(args);
+		commandArgs.parse(args);
 
-        String fileName = commandArgs.inputFile;
+		String fileName = commandArgs.inputFile;
 
 		if (fileName == null) {
 			System.exit(0);
@@ -68,13 +72,12 @@ public class Fcc {
 			System.exit(1);
 		}
 
-
 		Environment env = new Environment();
 		ArrayList<SemanticError> err = ast.checkSemantics(env);
 
-		if (err.size() > 0 ){
-			System.out.println("You had: " +err.size()+" error(s):");
-			for(SemanticError e : err)
+		if (err.size() > 0) {
+			System.out.println("You had: " + err.size() + " error(s):");
+			for (SemanticError e : err)
 				System.out.println("\t" + e);
 
 			System.exit(1);
@@ -95,10 +98,10 @@ public class Fcc {
 
 			try {
 				// CODE GENERATION  prova.fool.asm
-				String code=ast.codeGeneration(); 
-				BufferedWriter out = new BufferedWriter(new FileWriter(fileName+".asm")); 
+				String code = ast.codeGeneration();
+				BufferedWriter out = new BufferedWriter(new FileWriter(fileName + ".asm"));
 				out.write(code);
-				out.close(); 
+				out.close();
 			} catch (IOException e) {
 				System.out.println(e.toString());
 				System.exit(2);
@@ -107,5 +110,68 @@ public class Fcc {
 		}
 		System.exit(0);
 	}
-}
 
+	public static class Cli {
+		public boolean codeGen = true;
+		public String inputFile = null;
+		public boolean verbose = false;
+		private Options options;
+
+		public void parse(String[] args) {
+			options = new Options();
+			options.addOption("h", "help", false, "show this help menu.");
+			options.addOption("c", "check", false, "only perform semantic and type check");
+			options.addOption("d", "debug", false, "verbose output (parse tree)");
+			options.addOption("v", "version", false, "compiler version");
+			options.addOption("f", "input-file", true, "input file to be compilated");
+			CommandLineParser parser = new DefaultParser();
+
+			CommandLine cmd = null;
+			try {
+				cmd = parser.parse(options, args);
+				boolean option = false;
+
+				if (cmd.hasOption("h")) {
+					help();
+					option = true;
+				}
+
+				if (cmd.hasOption("c")) {
+					codeGen = false;
+					option = true;
+				}
+
+				if (cmd.hasOption("d")) {
+					verbose = true;
+					option = true;
+				}
+
+				if (cmd.hasOption("f")) {
+					String input = cmd.getOptionValue("f");
+					option = true;
+					if (input != null) {
+						inputFile = input;
+					} else {
+						System.out.println("Filename not specified");
+						System.exit(1);
+					}
+				}
+
+				if (!option) {
+					help();
+				}
+
+			} catch (ParseException e) {
+				help();
+			}
+		}
+
+		private void help() {
+			// This prints out some help
+			HelpFormatter formater = new HelpFormatter();
+
+			formater.printHelp("Main", options);
+		}
+	}
+
+}
