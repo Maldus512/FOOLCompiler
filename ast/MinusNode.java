974c9a8b171fd987d2e124d1eb9a01dd80bc0d7e
Carlo Stomeo <stomcarlo@gmail.com>
2017-07-07 15:20:42 +0200

    added DivNode and MinusNode

---
+++ ast/MinusNode.java
@@ -0,0 +1,54 @@
+package ast;
+
+import java.util.ArrayList;
+
+import util.Environment;
+import util.SemanticError;
+import lib.FOOLlib;
+
+import ast.types.*;
+
+public class MinusNode implements Node {
+
+    private Node left;
+    private Node right;
+
+    public MinusNode (Node l, Node r) {
+        left=l;
+        right=r;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        //create the result
+        ArrayList<SemanticError> res = new ArrayList<SemanticError>();
+
+        //check semantics in the left and in the right exp
+
+        res.addAll(left.checkSemantics(env));
+        res.addAll(right.checkSemantics(env));
+
+        return res;
+    }
+
+    public String toPrint(String s) {
+        return s+"Minus\n" + left.toPrint(s+"  ")  
+            + right.toPrint(s+"  ") ; 
+    }
+
+    public TypeNode typeCheck(Environment env) {
+        if (! ( FOOLlib.isSubtype(left.typeCheck(env),new IntTypeNode()) &&
+                    FOOLlib.isSubtype(right.typeCheck(env),new IntTypeNode()) ) ) {
+            System.out.println("Non integers in subtraction");
+            return new BoolTypeNode();
+        }
+        return new IntTypeNode();
+    }
+
+    public String codeGeneration() {
+        return left.codeGeneration()+
+            right.codeGeneration()+
+            "sub\n";
+    }
+
+}  
